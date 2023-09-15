dccff5018fccf0373195a6c3d594f087a38eb000
billybarnie <haefguafe@gmail.com>
2023-09-14 23:50:38 -0500

    updating course page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+let currentDate = new Date().getFullYear();
+document.querySelector('#year').textContent = currentDate;
+
+let dateModified = new Date();
+document.querySelector('#lastmodified').textContent = dateModified;
