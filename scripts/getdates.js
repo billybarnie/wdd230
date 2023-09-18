bae90d6431b8b81a31d2176a4534c96d391605a3
billybarnie <haefguafe@gmail.com>
2023-09-18 09:17:00 -0500

    edit 2 of last modified date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ let currentDate = new Date().getFullYear();
 document.querySelector('#year').textContent = currentDate;
 
 let lastModified = new Date(document.lastModified);
-document.querySelector('#lastmodified').textContent = lastModified;
+document.getElementById('#lastmodified').innerHTML = lastModified;
