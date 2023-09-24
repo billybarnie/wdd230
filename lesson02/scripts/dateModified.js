64cff371271c316ddd4650ab2a5c8583cce96bef
billybarnie <haefguafe@gmail.com>
2023-09-23 21:26:16 -0500

    final assignment update

--- lesson02/scripts/dateModified.js
+++ lesson02/scripts/dateModified.js
@@ -1,2 +1,5 @@
 let lastModified = new Date(document.lastModified);
 document.getElementById('lastmodified').textContent = lastModified;
+
+let currentDate = new Date().getFullYear();
+document.querySelector('#year').textContent = currentDate;
