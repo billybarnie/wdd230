ef715b9f3d6f8f272c72ee990d48ab4a40bee1df
billybarnie <haefguafe@gmail.com>
2023-10-16 23:38:26 -0500

    commiting

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,13 +1,30 @@
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+let modeButton = document.querySelector("#mode");
+let main = document.querySelector("main");
+//let section = document.querySelectorAll("section");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
+        const lights = document.getElementsByClassName('light');
+        console.log(lights.length);
+        console.log(lights);
+        let i = 0;
+        for(i=0; i<lights.length; i++) { 
+            //section.style.background = "darkgrey";
+            console.log(lights[i]);
+            lights[i].classList.replace("light","dark");
+        };
+        
 		main.style.background = "#000";
 		main.style.color = "#fff";
 		modeButton.textContent = "🔆";
 	} else {
-        section.style.background = "white";
+        const darks = document.getElementsByClassName('dark');
+        let i = 0;
+        for(i=0; i<darks.length; i++) { 
+            //section.style.background = "white";
+            darks[i].classList.replace("dark","light");
+        };
+        //section.style.background = "#FFBCC1";
 		main.style.background = "#FFBCC1";
 		main.style.color = "#000";
 		modeButton.textContent = "🕶️";
