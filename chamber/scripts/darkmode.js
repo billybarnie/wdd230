ab5a03b92e6efd770bca3092be1efa820db5fbc8
billybarnie <haefguafe@gmail.com>
2023-10-17 19:48:09 -0500

    making more updates

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -5,26 +5,34 @@ let main = document.querySelector("main");
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
         const lights = document.getElementsByClassName('light');
-        console.log(lights.length);
-        console.log(lights);
-        let i = 0;
-        for(i=0; i<lights.length; i++) { 
-            //section.style.background = "darkgrey";
-            console.log(lights[i]);
-            lights[i].classList.replace("light","dark");
-        };
         
+        let i = lights.length-1; 
+
+        for(i; i>=0; i--) {  
+
+            //section.style.background = "darkgrey"; 
+
+            console.log(lights[i]); 
+
+            lights[i].classList.replace("light","dark"); 
+
+        };
 		main.style.background = "#000";
 		main.style.color = "#fff";
 		modeButton.textContent = "🔆";
 	} else {
         const darks = document.getElementsByClassName('dark');
-        let i = 0;
-        for(i=0; i<darks.length; i++) { 
-            //section.style.background = "white";
-            darks[i].classList.replace("dark","light");
+        let i = darks.length-1; 
+
+        for(i; i>=0; i--) {  
+
+            //section.style.background = "darkgrey"; 
+
+            console.log(darks[i]); 
+
+            darks[i].classList.replace("dark","light"); 
+
         };
-        //section.style.background = "#FFBCC1";
 		main.style.background = "#FFBCC1";
 		main.style.color = "#000";
 		modeButton.textContent = "🕶️";
