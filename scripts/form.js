3f719b990e9fcdd7186a86a04d12312eda81e3af
billybarnie <haefguafe@gmail.com>
2023-11-02 23:37:08 -0500

    updating files

---
+++ scripts/form.js
@@ -0,0 +1,41 @@
+const kp1 = document.querySelector("#pwd");
+const kp2 = document.querySelector("#pwd1");
+const email = document.querySelector("#email");
+const rangeValue = document.querySelector("#rangevalue");
+const range = document.querySelector("#rate");
+
+kp2.addEventListener("focusout", checkSame);
+email.addEventListener("focusout", checkValid);
+range.addEventListener("change", displayRate);
+
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+
+		kp2.style.backgroundColor = "#fff0f3";
+        kp1.value = "";
+		kp2.value = "";
+		kp1.focus();
+
+	} else {
+
+		kp2.style.backgroundColor = "lightgreen";
+		kp2.style.color = "#000";
+	}
+}
+
+function checkValid() {
+    if (!email.contains("@byui.edu")) {
+
+        email.style.backgroundColor = "#fff0f3";
+        email.value = "";
+        email.focus();
+
+    }
+}
+
+function displayRate() {
+
+    rangeValue.textContent = range.value;
+    
+}
+
