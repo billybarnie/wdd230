edf49920d4c0b1589ca30c336138e38f5aa0a778
billybarnie <haefguafe@gmail.com>
2023-11-09 22:25:54 -0600

    updating

--- scripts/links.js
+++ scripts/links.js
@@ -1,6 +1,10 @@
 const baseURL = "https://billybarnie.github.io/wdd230/";
 const linksURL = "https://billybarnie.github.io/wdd230/data/links.json";
 
+const section = document.querySelector(".card");
+const ul = document.querySelector("ul");
+const h3 = document.querySelector("h3");
+
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
@@ -13,4 +17,22 @@ async function getLinks() {
     const data = await response.json();
     displayLinks(data);
 }
-  
+
+const displayLinks = (data) => {
+    data.lessons.forEach(lesson => {
+        lesson.links.forEach(link => {
+
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+
+            a.setAttribute("href", link.url);
+            a.setAttribute("target", "_blank");
+            a.textContent = `${lessons.lesson} : ${link.title}`
+
+            li.appendChild(a);
+            ul.appendChild(li);
+        });
+        
+    });
+    section.appendChild(ul);
+}
