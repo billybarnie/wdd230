7901956653ac7ffe873492f2e709a6b9c59baffe
billybarnie <haefguafe@gmail.com>
2023-11-09 23:32:04 -0600

    editting the json file

--- scripts/links.js
+++ scripts/links.js
@@ -1,20 +1,12 @@
-const baseURL = "https://billybarnie.github.io/wdd230/";
-const linksURL = "https://billybarnie.github.io/wdd230/data/links.json";
+const linksURL = "data/links.json";
 
-const section = document.querySelector(".card");
-const ul = document.querySelector("ul");
-const h3 = document.querySelector("h3");
+const section = document.querySelector(".lessons");
+const h3 = document.createElement("h3");
+const ul = document.createElement("ul");
 
-h3.textContent = "Learning activities";
+h3.textContent = "Learning Activities";
 
 section.appendChild(h3);
-
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-}
-  
-getLinks();
 
 async function getLinks() {
     const response = await fetch(linksURL);
@@ -25,20 +17,18 @@ async function getLinks() {
 const displayLinks = (data) => {
     data.lessons.forEach(lesson => {
         lesson.links.forEach(link => {
-
             let li = document.createElement("li");
             let a = document.createElement("a");
 
             a.setAttribute("href", link.url);
             a.setAttribute("target", "_blank");
-            a.textContent = `${lessons.lesson} : ${link.title}`
+            a.textContent = `${lesson.lesson}: ${link.title}`;
 
             li.appendChild(a);
             ul.appendChild(li);
         });
-        
     });
     section.appendChild(ul);
 }
 
-displayLinks()
+getLinks();
