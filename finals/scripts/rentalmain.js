f12013367fb490e0dd5137031bffca0d20977c0b
billybarnie <haefguafe@gmail.com>
2023-12-05 03:35:50 -0600

    reservations page update

---
+++ finals/scripts/rentalmain.js
@@ -0,0 +1,33 @@
+fetch('data/rentalprices.json')
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+})
+.then(rentalsData => {
+    function createRentalSection(rental) {
+        const section = document.createElement('section');
+        const image = document.createElement('img');
+        const rentalInfo = document.createElement('div');
+
+        image.src = rental.imageURL;
+        image.alt = rental['Rental Type'];
+
+        for (const key in rental) {
+            if (key !== 'imageURL') {
+                const p = document.createElement('p');
+                p.textContent = `${key}: ${rental[key]}`;
+                rentalInfo.appendChild(p);
+            }
+        }
+
+        section.appendChild(image);
+        section.appendChild(rentalInfo);
+
+        document.getElementById('maindiv').appendChild(section);
+    }
+
+    rentalsData.rentals.forEach(createRentalSection);
+})
+.catch(error => console.error('Error fetching or parsing JSON:', error));
---
+++ finals/scripts/rentaltabledata.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    fetch('data/rentalprices.json')
+      .then(response => response.json())
+      .then(data => {
+
+        const rentals = data.rentals;
+
+        const tableBody = document.querySelector('#rentalTable tbody');
+
+        rentals.forEach(rental => {
+          const row = document.createElement('tr');
+
+          const rentalTypeCell = document.createElement('td');
+          rentalTypeCell.textContent = rental['Rental Type'];
+          row.appendChild(rentalTypeCell);
+  
+          const maxPersonsCell = document.createElement('td');
+          maxPersonsCell.textContent = rental['Max. Persons'];
+          row.appendChild(maxPersonsCell);
+  
+          const reservationHalfDayCell = document.createElement('td');
+          reservationHalfDayCell.textContent = `$${rental['Reservation - Half Day']}`;
+          row.appendChild(reservationHalfDayCell);
+  
+          const reservationFullDayCell = document.createElement('td');
+          reservationFullDayCell.textContent = `$${rental['Reservation - Full Day']}`;
+          row.appendChild(reservationFullDayCell);
+  
+          const walkInHalfDayCell = document.createElement('td');
+          walkInHalfDayCell.textContent = `$${rental['Walk-In - Half Day']}`;
+          row.appendChild(walkInHalfDayCell);
+  
+          const walkInFullDayCell = document.createElement('td');
+          walkInFullDayCell.textContent = `$${rental['Walk-In - Full Day']}`;
+          row.appendChild(walkInFullDayCell);
+
+          tableBody.appendChild(row);
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+});
