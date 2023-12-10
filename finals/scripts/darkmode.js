73213e04ca516558a3ad76ab06bbaaf82d988949
billybarnie <haefguafe@gmail.com>
2023-12-10 01:54:04 -0600

    updating pages to toggle

--- finals/scripts/darkmode.js
+++ finals/scripts/darkmode.js
@@ -2,13 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const switchBox = document.querySelector('.switch input[type="checkbox"]');
     const body = document.querySelector('main');
 
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+    if (isDarkMode) {
+        switchBox.checked = true;
+        enableDarkMode();
+    }
+
     switchBox.addEventListener('change', () => {
         if (switchBox.checked) {
-            body.style.background = 'black';
-            body.style.color = 'white';
+            enableDarkMode();
+
+            localStorage.setItem('darkMode', 'true');
         } else {
-            body.style.background = 'white';
-            body.style.color = 'black';
+            disableDarkMode();
+
+            localStorage.setItem('darkMode', 'false');
         }
     });
+
+    function enableDarkMode() {
+        body.style.background = 'black';
+        body.style.color = 'white';
+    }
+
+    function disableDarkMode() {
+        body.style.background = 'white';
+        body.style.color = 'black';
+    }
 });
---
+++ finals/scripts/toggled.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var currentPage = window.location.href;
+
+    var navLinks = document.querySelectorAll(".navigate a");
+
+    navLinks.forEach(function (link) {
+        if (link.href === currentPage) {
+
+            link.classList.add("current-page");
+        }
+    });
+});
